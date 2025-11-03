3603d3412496eb1d9d273a4fa70b86f3024ab7ad
Augusto Sousa <augustosousa1798@gmail.com>
2025-11-03 05:08:05 -0400

    requirements att

--- voz_teste.py
+++
@@ -1,9 +0,0 @@
-import speech_recognition as sr
-
-recognizer = sr.Recognizer()
-with sr.Microphone() as source:
-    print('Diga seu comando:')
-    audio = recognizer.listen(source)
-    comando = recognizer.recognize_google(audio, language='pt-BR')
-    print('Você disse:', comando)
-    
